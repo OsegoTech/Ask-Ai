f09ea235643c62ce47d5dda1f94236fc54bc1671
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-11-28 18:33:03 +0300

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import TextView from '../views/TextView.vue'
+import AudioView from '../views/AudioView.vue'
+import ImageView from '../views/ImageView.vue'
+import { useTokenizeStore } from '../stores/tokenize'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/text',
+      name: 'text',
+      component: TextView
+    },
+    {
+      path: '/audio',
+      name: 'audio',
+      component: AudioView
+    },
+    {
+      path: '/image',
+      name: 'image',
+      component: ImageView
+    }
+  ]
+})
+
+router.beforeEach(() => {
+  const tokenizeStore = useTokenizeStore()
+  tokenizeStore.tokenLength = 0
+})
+
+export default router
